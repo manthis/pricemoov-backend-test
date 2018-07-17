717b6ae07c2b9f8e530e430420296da8a6062204
Maxime AUBURTIN <maxime.auburtin@dockermail.io>
2018-07-17 22:46:42 +0200

    Step 3 ready

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,36 +4,38 @@ import jwt from 'jsonwebtoken';
 import User from '../models/user';
 import config from '../config/config';
 
-const basic_auth = async (req, res) => {
+const basic_auth = (req, res, callback) => {
     const user = basicAuth(req);
     if (!user || !user.name || !user.pass) {
-        return false;
+        callback(false);
+        return;
     }
-    await User.findOne({ login: user.name }, (err, dbUser) => {
+    User.findOne({ login: user.name }, (err, dbUser) => {
         if (err || !dbUser) {
             if (err) console.log(err);
-            return false;
+            callback(false);
+            return;
         }
-        bcrypt.compare(user.pass, dbUser.password, (err, success) => {
-            if (err || !success) {
-                return false;
-            }
-            return true;
-        });
+        callback(bcrypt.compareSync(user.pass, dbUser.password));
     });
 }
 
 const jwt_auth = (req) => {
     try {
         const access_token = req.headers['x-access-token'];
+        if (!access_token) {
+            console.log('No JWT access token provided in headers!');
+            return false;
+        }
         return jwt.verify(access_token, config.JWT_SECRET);
     }
     catch (err) {
         /* We do not throw the error if jwt isn't specified to be able to use basic auth */
-        if (!(err.message === 'jwt must be provided')) {
+        if (err.message !== 'jwt must be provided') {
             console.log('Error during JWT auth: ' + err.message);
             throw err;
         }
+        return false;
     }
 }
 
@@ -41,20 +43,26 @@ const auth = (req, res, next) => {
     try {
         // If the client successfully auth with JWT
         if (jwt_auth(req)) {
+            console.log('Successfully authenticated using JWT!');
             next();
             return;
-        }
+        }   
         // If JWT auth is unsuccessful we try with basic auth
-        if (basic_auth(req, res)) {
-            next();
-            return;
-        }         
-        // Otherwise we request the user to authenticate
-        res.set('WWW-Authenticate', 'Authorization Required');
-        res.sendStatus(401);        
+        basic_auth(req, res, (is_authenticated) => {
+            if (is_authenticated === true) {
+                console.log('Successfully authenticated using Basic Auth!');
+                next();
+            } 
+            else {
+                // Otherwise we request the user to authenticate
+                console.log('Authentication Failure!');
+                res.set('WWW-Authenticate', 'Authorization Required');
+                res.sendStatus(401);  
+            }
+        });      
     }
     catch (err) {
-        res.json({ error: err });
+        console.log(err.message);
     }
 }
 
